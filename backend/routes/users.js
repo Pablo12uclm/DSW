5bc4d6136f1a0823ef5760e9793ec4f42db819f4
Pablo12uclm <115165900+Pablo12uclm@users.noreply.github.com>
2024-05-07 17:50:41 +0200

    Add files via upload

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+// routes/users.js
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController'); 
+const authController = require('../controllers/authController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/register', userController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
